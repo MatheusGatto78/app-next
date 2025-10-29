745622294af4b24314683cdc8ff39ba42632d064
MatheusGatto78 <matheusviniciusgatto@gmail.com>
2025-10-29 16:27:24 -0300

    ultimos ajustes

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
@@ -14,11 +16,38 @@ import {
   FieldLabel,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { redirect } from "next/navigation";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+
+  const [loading, setloading] = useState(false)
+  const [error, setError] = useState("")
+
+  function handleLogin(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email") as string;
+    const senha = formData.get("senha") as string;
+
+    authClient.signIn.email({
+      email: email,
+      password: senha
+    },
+    {
+      onSuccess: () => redirect("/dashboard"),
+      onRequest: () => setloading(true),
+      onResponse:() => setloading(false),
+      onError: (ctx) => setError(ctx.error.message)
+    }
+
+  )
+  }
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
